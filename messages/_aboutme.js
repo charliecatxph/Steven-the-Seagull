ea1362e3f260a0fa1875d9f6fa734d049224d1c6
Charl Concepcion <76478646+charliecatxph@users.noreply.github.com>
2022-08-24 01:36:15 +0800

    about me changes

--- messages/_aboutme.js
+++ messages/_aboutme.js
@@ -6,7 +6,7 @@ module.exports = function (vc) {
     .setColor(success)
     .setTitle("About Me :")
     .setDescription(
-      `Hi! I'm **Steven the Seagull**! I was made by a young developer with the codename **\`<charliecatxph/>\`** who really likes the **"Feeding Steven"** channel on YouTube! \n \n GitHub Link : https://github.com/charliecatxph \n Email : steventheseagull.bot@gmail.com \n \n Collaborators : **\`jellix_\`** \n \n Version : v1.9`
+      `Hi! I'm **Steven the Seagull**! I was made by a young developer with the codename **\`<charliecatxph/>\`** who really likes the **"Feeding Steven"** channel on YouTube! \n \n GitHub Link : \`https://github.com/charliecatxph\` \n Email : \`charliecatxph@gmail.com\` \n Developer : Charl Concepcion @charliecatxph \n Company : CTX Softwares Philippines \n \n Collaborators : **\`jellix_\`** \n \n Version : \`v1.9\` \n Streaming music since : \`Feb 14, 2022\``
     )
     .setAuthor({ name: "🌊🐦 Steven the Seagull" });
   vc.send({ embeds: [message] });
